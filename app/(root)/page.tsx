f7f33655128829c34f95d4cfac2a21c9f8d602cc
jincy-dev <jincydev3007@gmail.com>
2025-01-24 13:16:33 +0530

    fix type error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ProductList from "@/components/shared/poduct/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
+import { Product } from "@/types";
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
   return (
     <div>
       <ProductList
-        data={latestProducts}
+        data={latestProducts as unknown  as Product[]}
         title="Newest Arrival"
         limit={4}
       />
